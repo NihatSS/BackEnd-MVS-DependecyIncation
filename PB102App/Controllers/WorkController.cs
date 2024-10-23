29a217e17d040fc110e486948c8e0f9634e19ef9
User <nihatss@code.edu.az>
2024-10-23 23:37:51 +0400

    MVS connectionstring

---
+++ PB102App/Controllers/WorkController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PB102App.Data;
+using PB102App.Models;
+
+namespace PB102App.Controllers
+{
+    public class WorkController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public WorkController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Work> work = await _context.Works.ToListAsync();
+            return View(work);
+        }
+
+        public async Task<IActionResult> Detail(int? id)
+        {
+            if (id is null) return BadRequest();
+            Work work = await _context.Works.Include(m=>m.Category).FirstOrDefaultAsync(x => x.Id == id);
+            if (work == null) return NotFound();
+            return View(work);
+        }
+    }
+}
